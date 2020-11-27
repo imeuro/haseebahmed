cf88d702148ad9f8297895bf669c7fd920979761
imeuro <mauro.fioravanzi@gmail.com>
2020-11-28 00:00:02 +0100

    test lightbox in mobile

--- HA-content/themes/havenicewp/js/common.js
+++ HA-content/themes/havenicewp/js/common.js
@@ -35,7 +35,7 @@ if (bodyClassList.contains('home')) {
 		touchNavigation: false,
 		keyboardNavigation: false,
 		width: '100vw',
-		height: '100vw',
+		height: '100vh',
 		loop: false,
 		draggable: false,
 		autoplayVideos: false,
