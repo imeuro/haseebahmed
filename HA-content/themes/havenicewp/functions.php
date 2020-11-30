959b45a96c7dfc64695473d460457bb57d8859bd
imeuro <mauro.fioravanzi@gmail.com>
2020-11-30 01:24:46 +0100

    major cleanup

--- HA-content/themes/havenicewp/functions.php
+++ HA-content/themes/havenicewp/functions.php
@@ -47,13 +47,6 @@ if ( ! function_exists( 'havenicewp_setup' ) ) :
 		 */
 		add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus(
-			array(
-				'menu-1' => esc_html__( 'Primary', 'havenicewp' ),
-				'menu-2' => esc_html__( 'Secondary', 'havenicewp' ),
-			)
-		);
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
@@ -72,70 +65,9 @@ if ( ! function_exists( 'havenicewp_setup' ) ) :
 			)
 		);
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'havenicewp_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support(
-			'custom-logo',
-			array(
-				'height'      => 250,
-				'width'       => 250,
-				'flex-width'  => true,
-				'flex-height' => true,
-			)
-		);
 	}
 endif;
 add_action( 'after_setup_theme', 'havenicewp_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function havenicewp_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'havenicewp_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'havenicewp_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function havenicewp_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'havenicewp' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'havenicewp' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'havenicewp_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -143,45 +75,26 @@ add_action( 'widgets_init', 'havenicewp_widgets_init' );
 function havenicewp_scripts() {
 	wp_enqueue_style( 'havenicewp-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'havenicewp-style', 'rtl', 'replace' );
-	wp_enqueue_style( 'flickity', 'https://unpkg.com/flickity@2/dist/flickity.min.css', array(), _S_VERSION);
-	wp_enqueue_style( 'glightbox', 'https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css', array(), _S_VERSION);
-	wp_enqueue_style( 'havenicewp-common', get_template_directory_uri() . '/common.css', array('havenicewp-style'), _S_VERSION);
+	wp_enqueue_style( 'flickity', get_template_directory_uri() . '/css/flickity.min.css', array(), _S_VERSION);
+	wp_enqueue_style( 'glightbox', get_template_directory_uri() . '/css/glightbox.min.css', array(), _S_VERSION);
+	wp_enqueue_style( 'havenicewp-common', get_template_directory_uri() . '/css/common.css', array('havenicewp-style'), _S_VERSION);
+	if (is_single()) {
+		wp_enqueue_style( 'flickity-fullscreen', get_template_directory_uri() . '/css/flickity-fullscreen.css', array('havenicewp-style'), _S_VERSION);
+	}
 
 
 	wp_enqueue_script( 'havenicewp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script( 'flickity', 'https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js', array(), '', true);
-	wp_enqueue_script( 'glightbox', 'https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js', array('flickity'), '', true);
+	wp_enqueue_script( 'flickity', get_template_directory_uri() . '/js/flickity.pkgd.min.js', array(), '', true);
+	wp_enqueue_script( 'glightbox', get_template_directory_uri() . '/js/glightbox.min.js', array('flickity'), '', true);
 	wp_enqueue_script( 'havenicewp-common', get_template_directory_uri() . '/js/common.js', array('glightbox'), _S_VERSION, true);
+	if (is_single()) {
+		wp_enqueue_script( 'havenicewp-common', get_template_directory_uri() . '/js/flickity-fullscreen.js', array('flickity'), _S_VERSION, true);
+	}
+
 
 }
 add_action( 'wp_enqueue_scripts', 'havenicewp_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
 
 /* custom functions for HAveniceWP theme */
